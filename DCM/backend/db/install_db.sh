ddd7be1c3a26d684f3dac9e4c6d36042bf06ace7
Reza Shahriari <reza_shahriari@yahoo.ca>
2019-10-14 21:08:42 -0400

    install db

---
+++ DCM/backend/db/install_db.sh
@@ -0,0 +1,5 @@
+brew install postgresql
+ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
+alias pg_start="launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist"
+alias pg_stop="launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist"
+creaatedb pace
