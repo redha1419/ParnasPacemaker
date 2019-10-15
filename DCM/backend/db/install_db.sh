5d647a6b4cd6d8fb4943d5bffe805c06f525a84d
Reza Shahriari <reza_shahriari@yahoo.ca>
2019-10-14 21:24:07 -0400

    typo

--- DCM/backend/db/install_db.sh
+++ DCM/backend/db/install_db.sh
@@ -2,4 +2,5 @@ brew install postgresql
 ln -sfv /usr/local/opt/postgresql/*.plist ~/Library/LaunchAgents
 alias pg_start="launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist"
 alias pg_stop="launchctl unload ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist"
-creaatedb pace
+launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
+createdb pace
