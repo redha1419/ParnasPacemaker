90aed5e765b38201ec6cd6f7aef435a276b93cd9
Reza Shahriari <reza_shahriari@yahoo.ca>
2019-10-28 17:00:29 -0400

    comments

--- DCM/backend/index.js
+++ DCM/backend/index.js
@@ -5,7 +5,7 @@ const app      = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//CORS
+//CORS, to make our server public even when using different location
  app.use((req, res, next)=> {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
