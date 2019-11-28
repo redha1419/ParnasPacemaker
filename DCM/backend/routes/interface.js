9d7c7e706a86773a974fff5017ad0cafb749d795
Nasar Khan <nasarak_786@hotmail.com>
2019-11-27 22:12:32 -0500

    added other pacing modes

--- DCM/backend/routes/interface.js
+++ DCM/backend/routes/interface.js
@@ -7,22 +7,12 @@ const SerialPort = require('serialport');
 const Readline = require('@serialport/parser-readline');
 const ByteLength = require('@serialport/parser-byte-length')
 const path = '/dev/tty.usbmodem0000001234561';        //path for K64F board
-const port = new SerialPort(path, { baudRate: 115200 });
+const port = new SerialPort(path, { baudRate: 115200, autoOpen: true});
+
 var buf = Buffer.alloc(54);
 var verify = Buffer.alloc(2);
 const parser = new Readline();
 
-/*
-router.get('/pacing_mode', function(req, res){
-
-    const port = new SerialPort(path, { baudRate: 256000 });
-    const parser = new Readline();
-    port.pipe(parser);
-    parser.on('data', line => console.log(`> ${line}`));
-    port.write('ROBOT POWER ON\n');
-
-});
-*/
 
 //setting the pace mode
 router.post('/pace', function(req, res){
@@ -32,7 +22,7 @@ router.post('/pace', function(req, res){
     let config = req.body.config;
 
 
-    knex('users') //query db and retrieve user
+    knex('users') //query db and retrieve [user]
     .where({username})
     .first()
     .then((user)=>{
@@ -46,24 +36,35 @@ router.post('/pace', function(req, res){
             console.log("HERE 1")
             payload = req.body
             console.log(payload.mode)
-
             getByteStream(payload);
             //set bytes
 
             console.log(buf)
+            
+            /*port.open(function (err) {
+                if (err) {
+                  return console.log('Error opening port: ', err.message)
+                }
+              
+                // Because there's no callback to write, write errors will be emitted on the port:
+                port.write(buf)
+                console.log(buf);
+                port.close();
+              })*/
              port.write(buf,function (err) {
                 if (err) {
                   return console.log('Error opening port: ', err.message)
                 }
               
                 // Because there's no callback to write, write errors will be emitted on the port:
-                //console.log(buf);
+                console.log(buf);
               })
+
               //console.log(buf);
 
               
               //verify = Buffer.from([0, 34]);
-              verify.writeInt8(0,0)
+              /*verify.writeInt8(0,0)
               verify.writeInt8(1,1)
               //verify = Buffer.from([0,34])
               console.log(verify);
@@ -71,14 +72,11 @@ router.post('/pace', function(req, res){
                 if (err) {
                   return console.log('Error opening port: ', err.message)
                 }
-                //port.pipe(parser)
-                //console.log(parser);
-                //parser.on('data', console.log)
-                //console.log(verify);
 
                 const parser = port.pipe(new ByteLength({length: 54}))
-                parser.on('data', console.log)
-            }) 
+                parser.on('data', console.log)      //to do: store data in a buffer for comparison
+                //parser.off();
+            }) */
 
             res.status(200).json({message: "succesfully saved configs for " + mode})
         })
@@ -94,10 +92,10 @@ router.post('/pace', function(req, res){
 });           
 
 function getByteStream (payload){
-    let sync = 0, fn = 0;     //22, 85
+    let sync = 0, fn = 1;     //22, 85
 
-    buf.writeUInt8(sync, 0);        //rxdata[0]
-    buf.writeUInt8(fn, 1);          //rxdata[1]
+    buf.writeInt8(sync, 0);        //rxdata[0]
+    buf.writeInt8(fn, 1);          //rxdata[1]
 
     /*buf.writeFloatBE(payload.config.upper);    //upper limit - rxdata[3:4]
     buf.writeFloatBE(payload.config.lower);*/
@@ -144,7 +142,7 @@ function getByteStream (payload){
         buf.writeFloatBE(0, 46);       //response time
         buf.writeFloatBE(0, 50);       //activity threshold
     }
-    else {   // VVI
+    else if (payload.mode == "VVA") {   // VVI
         buf.writeFloatBE(3, 10);   //mode
         buf.writeFloatBE(0, 14);   //arfp - rxdata[8:9]
         buf.writeFloatBE(payload.config.vrp, 18);  //vrfp - rxdata[10:11]
@@ -157,6 +155,84 @@ function getByteStream (payload){
         buf.writeFloatBE(0, 46);       //response time
         buf.writeFloatBE(0, 50);       //activity threshold
     }
+    else if (payload.mode == "VVI") {   // DOO
+        buf.writeFloatBE(5, 10);   //mode
+        buf.writeFloatBE(0, 14);   //arfp - rxdata[8:9]
+        buf.writeFloatBE(0, 18);  //vrfp - rxdata[10:11]
+        buf.writeFloatBE(payload.config.ventricular_pw, 22);     //pulse width - rxdata[12:13]
+        buf.writeFloatBE(0, 26);         //AV delay - rxdata[14:15]            250ms for dual pacing modes (DOO, DOOR)
+        buf.writeFloatBE(3900, 30) ; //atrial amplitude - rxdata[16:19]   convert to mv by *1000
+        buf.writeFloatBE(3900, 34) ;        //ventricular amplitude - rxdata [20:23]
+        buf.writeFloatBE(0, 38);       //MSR
+        buf.writeFloatBE(0, 42);       //reaction time
+        buf.writeFloatBE(0, 46);       //response time
+        buf.writeFloatBE(0, 50);       //activity threshold
+    }
+    /*else if (payload.mode == "VOOR") {   // VOOR
+        buf.writeFloatBE(6, 10);   //mode
+        buf.writeFloatBE(0, 14);   //arfp - rxdata[8:9]
+        buf.writeFloatBE(0, 18);  //vrfp - rxdata[10:11]
+        buf.writeFloatBE(-, 22);     //pulse width - rxdata[12:13]
+        buf.writeFloatBE(0, 26);         //AV delay - rxdata[14:15]            250ms for dual pacing modes (DOO, DOOR)
+        buf.writeFloatBE(0, 30) ; //atrial amplitude - rxdata[16:19]   convert to mv by *1000
+        buf.writeFloatBE(payload.config.ventricular_amp*1000, 34) ;        //ventricular amplitude - rxdata [20:23]
+        buf.writeFloatBE(-, 38);       //MSR
+        buf.writeFloatBE(-, 42);       //reaction time
+        buf.writeFloatBE(-, 46);       //response time
+        buf.writeFloatBE(-, 50);       //activity threshold
+    }
+    else if (payload.mode == "AOOR") {   // AOOR
+        buf.writeFloatBE(7, 10);   //mode
+        buf.writeFloatBE(0, 14);   //arfp - rxdata[8:9]
+        buf.writeFloatBE(0, 18);  //vrfp - rxdata[10:11]
+        buf.writeFloatBE(-, 22);     //pulse width - rxdata[12:13]
+        buf.writeFloatBE(0, 26);         //AV delay - rxdata[14:15]            250ms for dual pacing modes (DOO, DOOR)
+        buf.writeFloatBE(payload.config.atrial_amp*1000, 30) ; //atrial amplitude - rxdata[16:19]   convert to mv by *1000
+        buf.writeFloatBE(0, 34) ;        //ventricular amplitude - rxdata [20:23]
+        buf.writeFloatBE(-, 38);       //MSR
+        buf.writeFloatBE(-, 42);       //reaction time
+        buf.writeFloatBE(-, 46);       //response time
+        buf.writeFloatBE(-, 50);       //activity threshold
+    }
+    else if (payload.mode == "VVIR") {   // VVIR
+        buf.writeFloatBE(8, 10);   //mode
+        buf.writeFloatBE(0, 14);   //arfp - rxdata[8:9]
+        buf.writeFloatBE(payload.config.vrp, 18);  //vrfp - rxdata[10:11]
+        buf.writeFloatBE(payload.config.ventricular_pw, 22);     //pulse width - rxdata[12:13]
+        buf.writeFloatBE(0, 26);         //AV delay - rxdata[14:15]            250ms for dual pacing modes (DOO, DOOR)
+        buf.writeFloatBE(0, 30) ; //atrial amplitude - rxdata[16:19]   convert to mv by *1000
+        buf.writeFloatBE(payload.config.ventricular_amp*1000, 34) ;        //ventricular amplitude - rxdata [20:23]
+        buf.writeFloatBE(-, 38);       //MSR
+        buf.writeFloatBE(-, 42);       //reaction time
+        buf.writeFloatBE(-, 46);       //response time
+        buf.writeFloatBE(-, 50);       //activity threshold
+    }
+    else if (payload.mode == "AAIR") {   // AAIR
+        buf.writeFloatBE(9, 10);   //mode
+        buf.writeFloatBE(payload.config.arp, 14);   //arfp - rxdata[8:9]
+        buf.writeFloatBE(0, 18);  //vrfp - rxdata[10:11]
+        buf.writeFloatBE(payload.config.atrial_pw, 22);     //pulse width - rxdata[12:13]
+        buf.writeFloatBE(0, 26);         //AV delay - rxdata[14:15]            250ms for dual pacing modes (DOO, DOOR)
+        buf.writeFloatBE(payload.config.atrial_amp*1000, 30) ; //atrial amplitude - rxdata[16:19]   convert to mv by *1000
+        buf.writeFloatBE(0, 34) ;        //ventricular amplitude - rxdata [20:23]
+        buf.writeFloatBE(-, 38);       //MSR
+        buf.writeFloatBE(-, 42);       //reaction time
+        buf.writeFloatBE(-, 46);       //response time
+        buf.writeFloatBE(-, 50);       //activity threshold
+    }
+    else if (payload.mode == "DOOR") {   // DOOR
+        buf.writeFloatBE(10, 10);   //mode
+        buf.writeFloatBE(0, 14);   //arfp - rxdata[8:9]
+        buf.writeFloatBE(0, 18);  //vrfp - rxdata[10:11]
+        buf.writeFloatBE(payload.config.ventricular_pw, 22);     //pulse width - rxdata[12:13]
+        buf.writeFloatBE(0, 26);         //AV delay - rxdata[14:15]            250ms for dual pacing modes (DOO, DOOR)
+        buf.writeFloatBE(-, 30) ; //atrial amplitude - rxdata[16:19]   convert to mv by *1000
+        buf.writeFloatBE(-, 34) ;        //ventricular amplitude - rxdata [20:23]
+        buf.writeFloatBE(-, 38);       //MSR
+        buf.writeFloatBE(-, 42);       //reaction time
+        buf.writeFloatBE(-, 46);       //response time
+        buf.writeFloatBE(-, 50);       //activity threshold
+    } */
 }
 
 
